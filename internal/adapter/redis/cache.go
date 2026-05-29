5a8020cea71b4fbdbc2beb2e91b623da12fa5113
auwwer-a11y <auwwer@icloud.com>
2026-05-29 15:44:30 +0500

    feat: add redis cache adapter

---
+++ internal/adapter/redis/cache.go
@@ -0,0 +1,27 @@
+package redis
+
+import (
+	"github.com/redis/go-redis/v9"
+	"context"
+	"time"
+)
+
+type Cache struct {
+	client *redis.Client
+}
+
+func NewCache(client *redis.Client) *Cache {
+	return &Cache{client: client}
+}
+
+func (c *Cache) Set(ctx context.Context, key string, value string) error {
+	return c.client.Set(ctx, key, value, 0).Err()
+}
+
+func (c *Cache) Get(ctx context.Context, key string) (string, error) {
+	return c.client.Get(ctx, key).Result()
+}
+
+func (c *Cache) Delete(ctx context.Context, key string) error {
+	return c.client.Del(ctx, key).Err()
+}
