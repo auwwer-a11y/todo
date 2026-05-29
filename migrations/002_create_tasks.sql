879fb2e945084abf3e88160de36db6b825299a2f
auwwer-a11y <auwwer@icloud.com>
2026-05-29 16:02:14 +0500

    feat: add database migrations

---
+++ migrations/002_create_tasks.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+CREATE TABLE tasks (
+    id TEXT PRIMARY KEY,
+    title TEXT NOT NULL,
+    "description" TEXT,
+    user_id TEXT NOT NULL,
+    deadline TIMESTAMPTZ,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    status TEXT NOT NULL DEFAULT 'pending',
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+)
+
+-- +goose Down
+DROP TABLE IF EXISTS tasks;
